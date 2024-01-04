0252f796b4cfedd34859373ce4acb42e7b27acbd
khushbu <khushbusoni475@gmail.com>
2024-01-04 14:44:57 +0530

    [khushbu] adding date range and find cheapes hotel

--- main/java/org/example/HotelReservation.java
+++ main/java/org/example/HotelReservation.java
@@ -2,24 +2,40 @@ package org.example;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Date;
-import java.util.*;
 
 public class HotelReservation {
     public String hotelName;
     public Double price;
-    public HotelReservation(String hotelName, Double price) {
+    public Date startDate;
+    public Date endDate;
+
+    public HotelReservation(String hotelName, Double price, Date startDate, Date endDate ) {
         this.hotelName = hotelName;
         this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
-    public static HotelReservation createHotel(String hotelName, Double price) {
-        return new HotelReservation(hotelName, price);
+    public static HotelReservation createHotel(String hotelName, Double price, Date startDate, Date endDate) {
+        return new HotelReservation(hotelName, price, startDate, endDate);
     }
+    public static HotelReservation cheapHotel(HashMap<String, HotelReservation>hm)
+    {
+        HotelReservation cheap=null;
+        for(HotelReservation hotel:hm.values())
+        {
+            if(cheap==null||hotel.price<cheap.price)
+                cheap=hotel;
+        }
+        return cheap;
+    }
+    HashMap<String, HotelReservation> hm;
     public static void main(String[] args) {
-        HotelReservation ob1 = createHotel("Lakewood", 110.0);
-        HotelReservation ob2 = createHotel("Bridgewood", 160.0);
-        HotelReservation ob3 = createHotel("Ridgewood", 210.0);
+        HotelReservation ob1 = createHotel("Lakewood", 110.0, parseDate("10/Sep/2020"), parseDate("11/Sep/2020"));
+        HotelReservation ob2 = createHotel("Bridgewood", 160.0,parseDate("10/sep/2020"), parseDate("11/sep/2020"));
+        HotelReservation ob3 = createHotel("Ridgewood", 210.0,parseDate("10/sep/2020"), parseDate("11/sep/2020"));
         HashMap<String, HotelReservation> hm = new HashMap<>();
         hm.put(ob1.hotelName, ob1);
         hm.put(ob2.hotelName, ob2);
@@ -28,5 +44,35 @@ public class HotelReservation {
             HotelReservation ob = entry.getValue();
             System.out.println(ob.hotelName + " " + ob.price);
         }
+        Date rangeStartDate = parseDate("10/sep/2020");
+        Date rangeEndDate = parseDate("11/sep/2020");
+        showHotelByDateRange(hm, rangeStartDate, rangeEndDate);
+        HotelReservation cheap=cheapHotel(hm);
+        if(cheap!=null)
+            System.out.println("Cheap Hotel: "+cheap.hotelName +" "+cheap.price);
+        else
+            System.out.println("No Hotels Found...!!!");
     }
+
+    public static void showHotelByDateRange(HashMap<String, HotelReservation> hm, Date startDate, Date endDate) {
+        for (HotelReservation hotel : hm.values()) {
+            if (isDateRangeOverlap(startDate, endDate, hotel.startDate, hotel.endDate)) {
+                System.out.println("Range: " + startDate + " - " + endDate + " " + hotel.hotelName + " " + hotel.price);
+            }
+        }
+    }
+
+    public static boolean isDateRangeOverlap(Date targetStartDate, Date targetEndDate, Date startDate, Date endDate) {
+        return (targetStartDate.compareTo(endDate) <= 0 && targetEndDate.compareTo(startDate) >= 0);
+    }
+    public static Date parseDate(String dateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy", Locale.ENGLISH);
+        try {
+            return sdf.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- test/java/HotelReservation_TestCase.java
+++ test/java/HotelReservation_TestCase.java
@@ -1,13 +1,44 @@
 import org.example.HotelReservation;
 import junit.framework.Assert;
 import org.junit.Test;
-
+import java.util.HashMap;
+import static org.example.HotelReservation.parseDate;
 public class HotelReservation_TestCase {
+    private HashMap<String, HotelReservation> hotelMap;
+
     @Test
-    public void getHotelReservation_Validation()
+    public void getHotelNameAndPrice_Validation()
     {
-        HotelReservation ob = new HotelReservation("Lakewood",110.0);//actual
+        HotelReservation ob = new HotelReservation("Lakewood",110.0, parseDate("10/Sep/2020"), parseDate("11/Sep/2020"));
         Assert.assertEquals("Lakewood", ob.hotelName);
         Assert.assertEquals(110.0, ob.price);
+        HotelReservation ob1 = new HotelReservation("Lakewood",160.0,parseDate("10/sep/2020"), parseDate("11/sep/2020"));
+        Assert.assertEquals("Lakewood", ob1.hotelName);
+        Assert.assertEquals(160.0, ob1.price);
+        HotelReservation ob2 = new HotelReservation("Lakewood",210.0, parseDate("10/Sep/2020"), parseDate("11/Sep/2020"));
+        Assert.assertEquals("Lakewood", ob2.hotelName);
+        Assert.assertEquals(210.0, ob2.price);
+    }
+    @Test
+    public void UC2_DateRangeCheapest()
+    {
+        HotelReservation ob1=new HotelReservation("Lakewood",110.0,parseDate("10/sep/2020"),parseDate("11/sep/2020"));//actual
+        HotelReservation ob2=new HotelReservation("Bridgewood", 160.0,parseDate("10/sep/2020"),parseDate("11/sep/2020"));//actual
+        HotelReservation ob3=new HotelReservation("Ridgewood", 210.0,parseDate("10/sep/2020"),parseDate("11/sep/2020"));//actual
+        if (ob1.price < ob2.price && ob1.price < ob3.price) {
+            Assert.assertEquals("Lakewood", ob1.hotelName);
+            Assert.assertEquals(110.0, ob1.price);
+            System.out.println("Hotel "+ob1.hotelName +" is Cheapest at Price: "+ob1.price+" Available from:- "+ob1.startDate+ " to "+ob1.endDate);
+        }
+        else if (ob2.price<ob1.price&&ob2.price<ob3.price) {
+            Assert.assertEquals("Bridgewood", ob2.hotelName);
+            Assert.assertEquals(160.0, ob2.price);
+            System.out.println("Hotel "+ob2.hotelName +" is Cheapest at Price: "+ob2.price+" Available from:- "+ob2.startDate+ " to "+ob2.endDate);
+        }
+        else if (ob3.price<ob1.price&&ob3.price<ob2.price) {
+            Assert.assertEquals("Ridgewood", ob3.hotelName);
+            Assert.assertEquals(210.0, ob3.price);
+            System.out.println("Hotel "+ob3.hotelName +" is Cheapest at Price: "+ob3.price+" Available from:- "+ob3.startDate+ " to "+ob3.endDate);
+        }
     }
 }
